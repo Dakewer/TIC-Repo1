c73f9e45566f9e4f6ede3ec73fba158053d8ce9d
David Páez <142960018+Dakewer@users.noreply.github.com>
2025-01-28 16:33:47 -0600

    Add files via upload

---
+++ common.h
@@ -0,0 +1,16 @@
+/*
+ * common.h
+ *
+ *  Created on: 23 oct. 2024
+ *      Author: hpiza
+ */
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+typedef enum {False, True} Bool;
+typedef void* Type;
+typedef void (*PrintFunc)(Type);
+typedef void (*FreeFunc)(Type);
+
+#endif /* COMMON_H_ */
